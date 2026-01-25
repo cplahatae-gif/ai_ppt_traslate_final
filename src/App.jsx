9f1dfd539626aa004022ab17b86c46bd4cbd3d3a
Ha <cplahatae@gmail.com>
2026-01-25 15:44:14 +0900

    feat: Vite 기반 React 프로젝트 초기 설정 및 번역 및 할 일 목록 컴포넌트 추가

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Translator from './components/Translator';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Translator />
+    </div>
+  );
+}
+
+export default App;
